aafa39eaed6f476f245e8c950704abeb170c786e
Kyle Edwards <kyedwards@nvidia.com>
2024-06-12 10:26:23 -0400

    Overhaul CI
    Model CI after rapids-build-backend, which publishes packages rather
    than just being a repository for pre-commit to clone.
    
    - Add DFG to pre-commit
    - Build Conda packages
    - Publish Conda packages and wheels
    - Enable copy-pr-bot

---
+++ ci/build_wheel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+python -m pip wheel . -w dist -vv --no-deps --disable-pip-version-check
+
+# Run tests
+WHL_FILE=$(ls dist/*.whl)
+python -m pip install "${WHL_FILE}[test]"
+python -m pytest -v tests/
+
+RAPIDS_PY_WHEEL_NAME="rapids-metadata" rapids-upload-wheels-to-s3 dist
