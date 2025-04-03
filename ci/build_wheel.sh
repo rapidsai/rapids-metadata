c235e60d495acb8c1dbd8ed007fc5c9639d5b4f9
VenkateshJaya <jjayabaskar@nvidia.com>
2025-04-03 16:18:58 -0700

    removed wheel_dir variable from ci scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,15 +3,11 @@
 
 set -euo pipefail
 
-wheel_dir="${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
-
-mkdir -p "${wheel_dir}"
-
-python -m pip wheel . -w "${wheel_dir}" -vv --no-deps --disable-pip-version-check
+python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disable-pip-version-check
 
 # Run tests
-WHL_FILE=$(ls "${wheel_dir}"/*.whl)
+WHL_FILE=$(ls "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
 
-RAPIDS_PY_WHEEL_NAME="rapids-metadata" rapids-upload-wheels-to-s3 python "${wheel_dir}"
+RAPIDS_PY_WHEEL_NAME="rapids-metadata" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
