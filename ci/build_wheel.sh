71421b1418dd094078f5a8061cd63eb8f13abdc9
James Lamb <jaylamb20@gmail.com>
2025-05-28 09:56:00 -0500

    stop uploading to downloads.rapids.ai, add shellcheck (#54)

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -9,5 +9,3 @@ python -m pip wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -vv --no-deps --disabl
 WHL_FILE=$(ls "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"/*.whl)
 python -m pip install "${WHL_FILE}[test]"
 python -m pytest -v tests/
-
-RAPIDS_PY_WHEEL_NAME="rapids-metadata" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
