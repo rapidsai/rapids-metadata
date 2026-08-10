703b907edee22e08b5cb99a825eec1cfe68d47af
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2026-08-10 10:29:31 -0700

    rapids-logger will no longer publish pre-releases (#64)
    See https://github.com/rapidsai/rapids-logger/pull/57. I've marked this PR as breaking since pre-commit hooks for repos will start failing as soon as this merges, so we'll need PRs ready to merge alongside this change.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rapids-metadata/pull/64

--- src/rapids_metadata/__init__.py
+++ src/rapids_metadata/__init__.py
@@ -354,6 +354,9 @@ all_metadata.versions["25.10"].repositories["cuvs-lucene"] = RAPIDSRepository(
 )
 
 all_metadata.versions["25.12"] = deepcopy(all_metadata.versions["25.10"])
+all_metadata.versions["25.12"].repositories["rapids-logger"].packages[
+    "rapids-logger"
+] = RAPIDSPackage(publishes_prereleases=False, has_cuda_suffix=False)
 del (
     all_metadata.versions["25.12"]
     .repositories["cugraph"]
