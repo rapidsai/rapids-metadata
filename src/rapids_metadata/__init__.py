a86e514c31c9dd77cb5fe742ca9d183d2a0e17f1
Michael Sarahan <msarahan@nvidia.com>
2026-08-31 12:12:08 -0500

    Add RAPIDS 26.12 metadata

--- src/rapids_metadata/__init__.py
+++ src/rapids_metadata/__init__.py
@@ -419,3 +419,5 @@ all_metadata.versions["26.08"].repositories["cudf"].packages["cudf-streaming"] =
     )
 )
 all_metadata.versions["26.10"] = deepcopy(all_metadata.versions["26.08"])
+
+all_metadata.versions["26.12"] = deepcopy(all_metadata.versions["26.10"])
